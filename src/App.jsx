bcf546b586f9351b7d736afccef06c3d8d3b42fc
Iorrah Mota <iorrah.gonzalez@gmail.com>
2020-07-11 14:03:01 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "styles.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <p>Hello World!</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+
+const domContainer = document.querySelector("#react-app");
+ReactDOM.render(e(App), domContainer);
